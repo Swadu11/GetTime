e647f2e7ee41463680bb848b4f48a7b2a27fd744
Swadeshwar K <swadeshwarko@gmail.com>
2025-03-17 13:34:15 +0530

    Add files via upload

---
+++ src/components/App.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    setCount(count + 1);
+  }
+  function decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={decrease}>-</button>
+      <button onClick={increase}>+</button>
+    </div>
+  );
+}
+export default App;
+
+// import React, { useState } from "react";
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   function increase() {
+//     setCount(count + 1);
+//   }
+
+//   function decrease() {
+//     setCount(count - 1);
+//   }
+
+// export default App;
